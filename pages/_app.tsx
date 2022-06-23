d9af5948493679b3c813f990ce11ccdc9cd63e86
Christopher Vestman <christophervestman@gmail.com>
2022-06-23 10:46:33 +0200

    feat: add custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/yaldevi';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Yaldevi, sans-serif',
+    body: 'Yaldevi, sans-serif',
+  },
+});
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />;
     </ChakraProvider>
   );
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sinhala Quiz</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='author' content='Christopher Vestman' />
+        <meta name='contact' content='contact@christophervestman.dev' />
+      </Head>
+      <h1>Quiz</h1>
+    </div>
+  );
+};
+
+export default HomePage;
