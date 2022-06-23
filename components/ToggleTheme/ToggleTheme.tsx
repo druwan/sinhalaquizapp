aa78a5c060b833f4f9c652f2ea3caa33351ff5c4
Christopher Vestman <christophervestman@gmail.com>
2022-06-23 15:44:10 +0200

    feat: Dark mode toggle, need to implement correct colors

---
+++ components/ToggleTheme/ToggleTheme.tsx
@@ -0,0 +1,32 @@
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { AnimatePresence, motion } from 'framer-motion';
+
+const ToggleTheme = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+  const colorScheme = colorMode === 'light' ? 'Raisin' : 'Turquoise';
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={colorScheme}
+        initial={{ y: 0, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 0, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label='Toggle theme'
+          colorScheme={colorScheme}
+          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+          onClick={toggleColorMode}
+          size={'md'}
+          pt={'2'}
+          variant={'link'}
+        />
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+export default ToggleTheme;
