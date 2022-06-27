ac3e68a33c2a2a7dac04eb76a40e2e04ee1edf1b
Christopher Vestman <christophervestman@gmail.com>
2022-06-27 09:23:17 +0200

    style: set maxWidth

--- components/Progress/TrackProgress.tsx
+++ components/Progress/TrackProgress.tsx
@@ -1,11 +1,15 @@
-import { Progress, useColorMode } from '@chakra-ui/react';
+import { Container, Progress, useColorMode } from '@chakra-ui/react';
 
 const TrackProgress = () => {
   const { colorMode } = useColorMode();
   const colorScheme = colorMode === 'light' ? 'Raisin' : 'Turquoise';
 
   // Calc new value based on num of questions answered.
-  return <Progress colorScheme={colorScheme} value={69} size={'sm'} />;
+  return (
+    <Container maxW={'container.sm'}>
+      <Progress colorScheme={colorScheme} value={69} size={'sm'} />
+    </Container>
+  );
 };
 
 export default TrackProgress;
