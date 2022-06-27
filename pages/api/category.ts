83746ceaebdf256091b9b52dfb4b042e6d1dcb9c
Christopher Vestman <christophervestman@gmail.com>
2022-06-27 16:05:33 +0200

    refactor

--- pages/api/category.ts
+++ pages/api/category.ts
@@ -1,5 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({categories: ['Alphabet', 'Family', 'Numbers', 'Pronouns', 'Time']})
+const categories = [
+  {id: 0, topic: 'Alphabet'},
+  {id: 1, topic: 'Family'},
+  {id: 2, topic: 'Numbers'},
+  {id: 3, topic: 'Pronouns'},
+  {id: 4, topic: 'Time'},
+]
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({categories: categories})
 }
