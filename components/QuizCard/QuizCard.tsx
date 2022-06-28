b3c0585ed254254707610a8544285a788c9ebcff
Christopher Vestman <christophervestman@gmail.com>
2022-06-28 10:53:32 +0200

    feat: centered to middle of screen

--- components/QuizCard/QuizCard.tsx
+++ components/QuizCard/QuizCard.tsx
@@ -1,48 +1,27 @@
-import {
-  Box,
-  Center,
-  Container,
-  Stack,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Center, Text, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
-import PrevNextCardBtn from '../Buttons/PrevNextCardBtn';
-import CategoryMenu from '../CategoryMenu/CategoryMenu';
 
 const QuizCard = () => {
   return (
-    <>
-      <Container>
-        {/* Move this into a 'start' page */}
-        <CategoryMenu />
-      </Container>
-
-      <Center py={6}>
-        <Box
-          maxW={'445px'}
-          w={'full'}
-          bg={useColorModeValue('Mint.300', 'Flax.200')}
-          boxShadow={'2xl'}
-          rounded={'md'}
-          p={6}
-          overflow={'hidden'}
-        >
-          <Stack>
-            <Text color='Raisin.900'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsa
-              maxime quod in aliquam aperiam rerum, tempora neque iste id odio,
-              vitae hic! Accusantium nam quo deleniti excepturi doloremque
-              earum.
-            </Text>
-          </Stack>
-        </Box>
-      </Center>
-
-      <Container as={'main'} maxW={'container.sm'}>
-        <PrevNextCardBtn />
-      </Container>
-    </>
+    <Center py={6}>
+      <Box
+        minW={'326px'}
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('Mint.300', 'Flax.200')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}
+        maxH={'400px'}
+        color='Raisin.900'
+      >
+        <Text fontSize={'2xl'} textAlign={'center'}>
+          එක
+        </Text>
+        Google translate btn for tips?
+      </Box>
+    </Center>
   );
 };
 
