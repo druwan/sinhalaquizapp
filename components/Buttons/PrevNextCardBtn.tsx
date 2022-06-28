db827a5e14a489d81afe3b5f7f0d27b23656550d
Christopher Vestman <christophervestman@gmail.com>
2022-06-28 10:34:31 +0200

    feat: extracted previous next btn to own component

---
+++ components/Buttons/PrevNextCardBtn.tsx
@@ -0,0 +1,24 @@
+import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
+import { Button, ButtonGroup, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+
+const PrevNextCardBtn = () => {
+  const { colorMode } = useColorMode();
+  const colorScheme = colorMode === 'light' ? 'Raisin' : 'Turquoise';
+
+  return (
+    <ButtonGroup
+      spacing={5}
+      display={'flex'}
+      alignContent={'center'}
+      justifyContent={'center'}
+      variant={'ghost'}
+      colorScheme={colorScheme}
+    >
+      <Button leftIcon={<ArrowLeftIcon />}>Previous Card</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Next Card</Button>
+    </ButtonGroup>
+  );
+};
+
+export default PrevNextCardBtn;
