946602f297782c132a8e6711df5689bfb76902b4
Christopher Vestman <christophervestman@gmail.com>
2022-06-28 12:10:48 +0200

    style: add color to nav heading

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,16 @@
-import { Container, Flex, Heading } from '@chakra-ui/react';
+import { Container, Flex, Heading, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 import ToggleTheme from '../ToggleTheme/ToggleTheme';
 
 const Navbar = () => {
+  const { colorMode } = useColorMode();
+  const textColor = colorMode === 'light' ? 'Raisin.900' : 'Flax.100';
   return (
     <Flex as={'nav'} p={3}>
       <Container maxW={'full'}>
-        <Heading as={'h2'}>Sinhala Quiz</Heading>
+        <Heading as={'h2'} color={textColor}>
+          Sinhala Quiz
+        </Heading>
       </Container>
 
       <Container maxW={'full'} centerContent alignItems={'end'}>
