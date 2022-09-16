00483f2eae20877e7a29629e0ac4fb3b2de8a7b8
Christopher Vestman <christophervestman@gmail.com>
2022-09-16 12:01:52 +0200

    refactor: adding navlinks to navbar to select category

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,28 @@
-import { Container, Flex, Heading, useColorMode } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
 import React from 'react';
 import ToggleTheme from '../ToggleTheme/ToggleTheme';
 
 const Navbar = () => {
   const { colorMode } = useColorMode();
   const textColor = colorMode === 'light' ? 'Raisin.900' : 'Flax.100';
+
+  const Links = ['Family', 'Numbers', 'Pronouns', 'Time'];
+
+  const NavLink = ({ children }) => (
+    <NextLink href='#' passHref>
+      <Link>{children}</Link>
+    </NextLink>
+  );
+
   return (
     <Flex as={'nav'} p={3}>
       <Container maxW={'full'}>
@@ -12,7 +30,13 @@ const Navbar = () => {
           Sinhala Quiz
         </Heading>
       </Container>
-
+      <Flex maxW={'full'} py={3}>
+        <Text fontSize={'md'}>
+          {Links.map((link) => (
+            <NavLink key={link}>{link}</NavLink>
+          ))}
+        </Text>
+      </Flex>
       <Container maxW={'full'} centerContent alignItems={'end'}>
         <ToggleTheme />
       </Container>
