0fc1699aa58ae86fcb7160388bc961bc0731d9e0
Christopher Vestman <christophervestman@gmail.com>
2022-09-18 17:40:03 +0200

    [WIP] - data fetching

--- pages/numbers.tsx
+++ pages/numbers.tsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import QuizCard from '../components/QuizCard/QuizCard';
+import { loadNumbers } from '../lib/loadQuestions';
 
-const numbers = () => {
+interface QuizItemObjType {
+  id: Number;
+  english: String;
+  sinhala: {
+    alphabetical: String;
+    script: String;
+  };
+}
+
+const Numbers = () => {
+  // Get all the Numbers
+  const [numbers, setNumbers] = useState<QuizItemObjType[]>([]);
+
+  useEffect(() => {
+    async function getNumbers() {
+      try {
+        const data = await loadNumbers();
+        setNumbers(data.props.questions);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getNumbers();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <QuizCard />
+      {/* <QuizCard /> */}
+      {numbers && console.log(Object.keys(numbers))}
     </>
   );
 };
 
-export default numbers;
+export default Numbers;
