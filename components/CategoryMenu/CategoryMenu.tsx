6aa76b2f06f2290513a81856fd19e9c12c5abf34
Christopher Vestman <christophervestman@gmail.com>
2022-09-21 11:49:11 +0200

    REFACTOR: removed links from navbar and added data fetching into the CategoryMenu this mean the numberspage is no longer needed

--- components/CategoryMenu/CategoryMenu.tsx
+++ components/CategoryMenu/CategoryMenu.tsx
@@ -10,57 +10,82 @@ import {
   Spacer,
   useColorMode,
 } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
+import { loadNumbers } from '../../lib/loadQuestions';
+import QuizCard from '../QuizCard/QuizCard';
 
 const fetcher = (...args: Parameters<typeof fetch>) =>
   fetch(...args).then((res) => res.json());
+
+interface Category {
+  id: number;
+  topic: string;
+}
 
 const CategoryMenu = () => {
   const { colorMode } = useColorMode();
   const colorScheme = colorMode === 'light' ? 'Mint' : 'Flax';
 
   const [selectTopic, setTopic] = useState('Select Category');
+  const [numbers, setNumbers] = useState(null);
 
   const { data, error } = useSWR('/api/category', fetcher);
 
   if (error) return <p>Failed to load</p>;
   if (!data) return <p>Loading...</p>;
 
+  useEffect(() => {
+    async function getQuizQuestions() {
+      try {
+        await loadNumbers().then((result) => setNumbers(result));
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    getQuizQuestions();
+  }, []);
+
   return (
-    <Center>
-      <HStack maxW={'container.md'}>
-        <Menu>
-          <MenuButton
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
+    <>
+      <Center>
+        <HStack py={6} maxW={'container.md'}>
+          <Menu>
+            <MenuButton
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+              colorScheme={colorScheme}
+            >
+              {selectTopic}
+            </MenuButton>
+            <MenuList>
+              {data.categories.map((category: Category) => (
+                <MenuItem
+                  key={category.id}
+                  value={category.topic}
+                  onClick={() => setTopic(category.topic)}
+                >
+                  {category.topic}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+
+          <Spacer />
+
+          <Button
             colorScheme={colorScheme}
+            onClick={() => setTopic('Select Category')}
           >
-            {selectTopic}
-          </MenuButton>
-          <MenuList>
-            {data.categories.map((category) => (
-              <MenuItem
-                key={category.id}
-                value={category.topic}
-                onClick={() => setTopic(category.topic)}
-              >
-                {category.topic}
-              </MenuItem>
-            ))}
-          </MenuList>
-        </Menu>
+            Clear Category
+          </Button>
+        </HStack>
+      </Center>
 
-        <Spacer />
-
-        <Button
-          colorScheme={colorScheme}
-          onClick={() => setTopic('Select Category')}
-        >
-          Clear Category
-        </Button>
-      </HStack>
-    </Center>
+      {selectTopic === 'Numbers' && (
+        <QuizCard props={numbers.props.numbers.numbers} />
+      )}
+    </>
   );
 };
 
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,26 +1,11 @@
-import {
-  Container,
-  Flex,
-  Heading,
-  Link,
-  Text,
-  useColorMode,
-} from '@chakra-ui/react';
-import NextLink from 'next/link';
+import { Container, Flex, Heading, useColorMode } from '@chakra-ui/react';
+
 import React from 'react';
 import ToggleTheme from '../ToggleTheme/ToggleTheme';
 
 const Navbar = () => {
   const { colorMode } = useColorMode();
   const textColor = colorMode === 'light' ? 'Raisin.900' : 'Flax.100';
-
-  const Links = ['Family', 'Numbers', 'Pronouns', 'Time'];
-
-  const NavLink = ({ children, href }) => (
-    <NextLink href={'/' + href} passHref>
-      <Link>{children}</Link>
-    </NextLink>
-  );
 
   return (
     <Flex as={'nav'} p={3}>
@@ -29,15 +14,6 @@ const Navbar = () => {
           Sinhala Quiz
         </Heading>
       </Container>
-      <Flex maxW={'full'} py={3}>
-        <Text fontSize={'md'}>
-          {Links.map((link) => (
-            <NavLink key={link} href={link}>
-              {link}
-            </NavLink>
-          ))}
-        </Text>
-      </Flex>
       <Container maxW={'full'} centerContent alignItems={'end'}>
         <ToggleTheme />
       </Container>
