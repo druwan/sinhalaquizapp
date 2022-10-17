b8910be5b4cbf5e5809292caabf17ce7a410106f
Christopher Vestman <christophervestman@gmail.com>
2022-10-17 17:31:49 +0200

    refactor: One api-fetch function

--- lib/loadQuestions.ts
+++ lib/loadQuestions.ts
@@ -1,43 +1,10 @@
-export async function loadFamily() {
-  const res = await fetch('http://localhost:3000/api/family');
-  const family = await res.json();
-
-  return {
-    props: {
-      family,
-    },
-  };
-}
-
-export async function loadNumbers() {
-  const res = await fetch('http://localhost:3000/api/numbers');
-  const numbers = await res.json();
-
-  return {
-    props: {
-      numbers,
-    },
-  };
-}
-
-export async function loadPronouns() {
-  const res = await fetch('http://localhost:3000/api/pronouns');
-  const pronouns = await res.json();
-
-  return {
-    props: {
-      pronouns,
-    },
-  };
-}
-
-export async function loadTime() {
-  const res = await fetch('http://localhost:3000/api/time');
+export async function loadApi(topic: string) {
+  const res = await fetch(`http://localhost:3000/api/${topic.toLowerCase()}`);
   const time = await res.json();
 
   return {
     props: {
-      time,
+      topic,
     },
   };
 }
