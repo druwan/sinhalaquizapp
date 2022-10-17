3b76e001ddbef1072eefaed603e48884cda2102b
Christopher Vestman <christophervestman@gmail.com>
2022-10-17 21:16:48 +0200

    lint

--- components/QuizCard/QuizAlternativesCard.tsx
+++ components/QuizCard/QuizAlternativesCard.tsx
@@ -4,30 +4,33 @@ import {
   Flex,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
-import React from 'react';
+} from "@chakra-ui/react";
+import React from "react";
 
-const QuizAlternativesCard = ({ correct, alternatives }) => {
+const QuizAlternativesCard = ({ correct, alternatives, currentScore }) => {
   const checkAnswer = (e) => {
     e.preventDefault();
-    if (e.target.innerText === correct[0].sinhala.alphabetical) {
-      console.log('Correct! Save score somehow...');
+
+    if (e.target.innerText === correct) {
+      currentScore += 1;
     } else {
-      console.log('Incorrect, move on to next question');
+      console.log(currentScore);
     }
+
+    // Next Question
   };
   return (
-    <Flex justifyContent={'space-around'} gap={3}>
+    <Flex justifyContent={"space-around"} gap={3}>
       <ButtonGroup>
-        {alternatives.map((alt) => (
+        {alternatives.map((alternative) => (
           <Button
-            key={alt.id}
-            size={'lg'}
-            bg={useColorModeValue('Mint.300', 'Flax.200')}
-            color={'Raisin.900'}
+            key={alternative.id}
+            size={"lg"}
+            bg={useColorModeValue("Mint.300", "Flax.200")}
+            color={"Raisin.900"}
             onClick={(e) => checkAnswer(e)}
           >
-            <Text fontSize={'xl'}>{alt.sinhala.alphabetical}</Text>
+            <Text fontSize={"xl"}>{alternative.sinhala.alphabetical}</Text>
           </Button>
         ))}
       </ButtonGroup>
