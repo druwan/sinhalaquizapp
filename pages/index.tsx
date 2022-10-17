943de9e8ca8105fe46243a12fbbfa6a73df0efd9
Christopher Vestman <christophervestman@gmail.com>
2022-10-17 21:16:05 +0200

    lint: and removed unused getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,36 @@
-import { Box, Flex } from '@chakra-ui/react';
-import Head from 'next/head';
+import { Box, Flex } from "@chakra-ui/react";
+import Head from "next/head";
 
-import CategoryMenu from '../components/CategoryMenu/CategoryMenu';
-import Navbar from '../components/Navbar/Navbar';
-
-import { loadNumbers } from '../lib/loadQuestions';
-
-export async function getStaticProps() {
-  const numbers = await loadNumbers();
-  return { props: { numbers } };
-}
+import CategoryMenu from "../components/CategoryMenu/CategoryMenu";
+import Navbar from "../components/Navbar/Navbar";
 
 const HomePage = () => {
   return (
-    <Box>
-      <Head>
-        <title>Sinhala Quiz</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='author' content='Christopher Vestman' />
-        <meta name='contact' content='contact@christophervestman.dev' />
-      </Head>
+    <>
+      <Box>
+        <Head>
+          <title>Sinhala Quiz</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="author" content="Christopher Vestman" />
+          <meta name="contact" content="contact@christophervestman.dev" />
+        </Head>
 
-      {/* Navbar */}
-      <Navbar />
-      <Flex
-        as={'main'}
-        flexDir={'column'}
-        alignItems={'center'}
-        minH={'75vh'}
-        justifyContent={'center'}
-      >
-        <CategoryMenu />
-      </Flex>
-    </Box>
+        {/* Navbar */}
+        <Navbar />
+        <Flex
+          as={"main"}
+          flexDir={"column"}
+          alignItems={"center"}
+          minH={"75vh"}
+          justifyContent={"center"}
+        >
+          <CategoryMenu />
+        </Flex>
+      </Box>
+    </>
   );
 };
 
